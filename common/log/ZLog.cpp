0927230cfea20dbf32b941b17e6be6e786ed1218
Aliceint <2464313547@qq.com>
2020-09-20 22:21:12 +0800

    增加日志写入文本的功能.后续待优化:考虑日志频繁开启关闭的问题和多线程的问题

--- common/log/ZLog.cpp
+++ common/log/ZLog.cpp
@@ -7,16 +7,83 @@
 #include <cstdarg>
 #include <vector>
 #include <sstream>
+#include <ctime>
+#include <iostream>
+#include <fstream>
 
-std::string LOG_MAP_[4]= {
+#ifdef __LINUX__
+#include <unistd.h> //access
+#include <sys/stat.h> //mkdir
+#include <errno.h> //errno
+#include <string.h> //strerror
+#endif
+
+std::string LOG_MAP_[5]= {
     "INVALID",
     "INFO",
     "WARNING",
-    "ERROR"
+    "USER ERROR",
+    "SYSTEM ERROR"
     };
+
+void logInfo(const char* file, const char* function, int line, const char *fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+    std::string res = LOGSTR(E_LOG_INFO, file, function, line, fmt, args);
+
+    pringLog(E_LOG_INFO, res);
+
+    va_end(args);
+}
+
+void logWarning(const char* file, const char* function, int line, const char *fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+
+    std::string res = logStr(E_LOG_WARNING, file, function, line, fmt , args);
+
+    pringLog(E_LOG_WARNING, res);
+
+    va_end(args);
+}
+
+void logError(const char* file, const char* function, int line, const char *fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+
+    std::string res = logStr(E_LOG_USER_ERROR, file, function, line, fmt , args);
+
+    pringLog(E_LOG_USER_ERROR, res);
+
+    va_end(args);
+}
+
+void sysError(const char* file, const char* function, int line, const char *fmt, ...)
+{
+    va_list args;
+
+    va_start(args, fmt);
+
+    std::string res = logStr(E_LOG_SYS_ERROR, file, function, line, fmt , args);
+
+    char sys_err_buf[1024];
+    sprintf(sys_err_buf, "%s", strerror(errno));
+
+    std::cout<< res << sys_err_buf <<std::endl;
+
+    va_end(args);
+    exit(1);
+}
 
 std::string logStr(E_LOG_LEVEL level, const char* file, const char* function, int line, const char *fmt, va_list args)
 {
+    //1. print msg
     va_list args_back;
 
     //All these functions(vprintf/vfprintf/vsprintf/vsnprintf) invoke va_arg at least once,
@@ -33,9 +100,15 @@ std::string logStr(E_LOG_LEVEL level, const char* file, const char* function, in
     std::vector<char> buf(1 + std::vsnprintf(nullptr, 0, fmt, args_back));
     std::vsnprintf(buf.data(), buf.size(), fmt, args);
 
+    //2. print time
+    std::time_t t = std::time(nullptr);
+    char time_buf[100];
+    std::strftime(time_buf, sizeof(time_buf), "%F %T", std::gmtime(&t));
+
+
 
     std::stringstream stream;
-    stream << "[" << LOG_MAP_[(int)level] <<"]"<<file<<":"<<line<<", INFO:"<<buf.data();
+    stream <<"["<<time_buf<<"] "<<"[" << LOG_MAP_[(int)level] <<"] "<<file<<":"<<line<<", INFO:"<<buf.data();
 
     std::string results;
     results = stream.str();
@@ -43,15 +116,49 @@ std::string logStr(E_LOG_LEVEL level, const char* file, const char* function, in
     return results;
 }
 
-void logInfo(const char* file, const char* function, int line, const char *fmt, ...)
+
+
+void pringLog(E_LOG_LEVEL level, std::string log)
 {
-    va_list args;
+    if (level >= PRINT_LOG_LEVEL)
+    {
+        if (PRINT_LOG_WAY == E_LOG_PRINT_STDOUT)
+        {
+            std::cout<<log<<std::endl;
+        }
+        else
+        {
+            std::string log_path = "./log";
+            #ifdef __LINUX__
+            if (access (log_path.c_str(), F_OK) != 0)
+            {
+                std::cout<<"dir is not exist"<<std::endl;
+                int b_ret = mkdir (log_path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+                if (b_ret == 0)
+                {
+                    std::cout<<"log dir is created"<<std::endl;
+                }
+                else
+                {
+                    SYSERROR("log dir is created failed");
+                }
+            }
+            #endif
 
-    va_start(args, fmt);
-    std::string res = LOGSTR(E_LOG_INFO, file, function, line, fmt, args);
+            #ifdef __WINDOWS_
 
-    std::cout<<res<<std::endl;
 
-    va_end(args);
+            #endif
+
+            std::time_t t = std::time(nullptr);
+            char time_buf[20];
+            strftime(time_buf, sizeof(time_buf), "%F", std::gmtime(&t));
+            std::string log_file = log_path + "/" + time_buf;
+            log_file += ".log";
+
+            std::ofstream ofs(log_file, std::ios::binary | std::ios::app);
+            ofs<<log<<"\n";
+            ofs.close();
+        }
+    }
 }
-
--- common/log/ZLog.h
+++ common/log/ZLog.h
@@ -5,22 +5,41 @@
  */
 #pragma once
 
-#include <iostream>
 #include <string>
+#define __LINUX__ 1
 
 enum E_LOG_LEVEL
 {
     E_LOG_INVALID = 0,
     E_LOG_INFO,
     E_LOG_WARNING,
-    E_LOG_ERROR
+    E_LOG_USER_ERROR,
+    E_LOG_SYS_ERROR
 };
+
+enum E_LOG_PRINT_WAY
+{
+    E_LOG_PRINT_INVALID = 0,
+    E_LOG_PRINT_STDOUT,
+    E_LOG_PRINT_TXT
+};
+
+//日志文件配置选项, 后续从配置文件读取,待优化.
+#define PRINT_LOG_LEVEL 1
+#define PRINT_LOG_WAY E_LOG_PRINT_TXT
 
 
 #define LOGINFO(fmt, ...) logInfo(__FILE__,__FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOGWARNING(fmt, ...) logWarning(__FILE__,__FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOGERROR(fmt, ...) logError(__FILE__,__FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define SYSERROR(fmt, ...) sysError(__FILE__,__FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
 #define LOGSTR(level, file, function, line, fmt, args) logStr(level, file, function, line, fmt, args)
 
 
 std::string logStr(E_LOG_LEVEL level, const char* file, const char* function, int line, const char *fmt, va_list args);
 void logInfo(const char* file, const char* function, int line, const char *fmt, ...);
+void logWarning(const char* file, const char* function, int line, const char *fmt, ...);
+void logError(const char* file, const char* function, int line, const char *fmt, ...);
+void sysError(const char* file, const char* function, int line, const char *fmt, ...);
+void pringLog(E_LOG_LEVEL level, std::string log);
 
